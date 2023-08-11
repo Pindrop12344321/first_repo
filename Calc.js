c4859bd04fd8272cd98ed15569708f4aa1f1e818
fokla <106857427+Pindrop12344321@users.noreply.github.com>
2023-08-11 18:03:13 +0530

    Add files via upload

---
+++ Calc.js
@@ -0,0 +1,14 @@
+ // audio
+      
+const audio = new Audio();
+audio.src = "https://cdn.pixabay.com/download/audio/2022/01/07/audio_5aa22f146f.mp3?filename=rclick-13693.mp3";
+
+      
+      // video
+
+function vibrateDevice() {
+      if (navigator.vibrate) {
+        navigator.vibrate([50, 25, 50]); 
+      }
+    }      
+      
